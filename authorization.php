96590dee8629f4a74539a34f5e6cbe1fe89d7b73
marevo <marevo1972@gmail.com>
2018-11-03 13:13:34 +0200

    fixed buggs for view form authorization in authorization.php

--- authorization.php
+++ authorization.php
@@ -7,7 +7,7 @@
  */
 ?>
 <script> $('#loginForm').remove(); </script>
-	<div id="loginForm"  style="margin-top: 20px;" class="col-xs-6 col-sm-6 col-lg-6 col-lg-offset-3  co-md-offset-3 text-center ">
+	<div id="loginForm"  style="margin-top: 20px;" class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-md-6 col-lg-offset-3  col-md-offset-3 text-center ">
 		<div class="panel panel-primary text-center">
 			<div class="panel-heading">
 				<h3 class="panel-title">
